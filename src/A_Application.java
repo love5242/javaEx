d3cdfb504f7e04b17fab8556d14aecb63caf2066
지후 <momo.class>
2024-07-16 16:28:27 +0900

    변수의 타입

---
+++ src/A_Application.java
@@ -0,0 +1,9 @@
+public class A_Application {
+
+
+    public static void main(String[] args) {
+    VariableType v = new VariableType();
+        
+    }
+
+}
---
+++ src/VariableType.java
@@ -0,0 +1,47 @@
+public class VariableType {
+
+
+
+public void type4(){
+
+
+
+    String str = "\"문자열\"";
+
+}
+
+int i = 10;
+//2진수 
+int ii = 0b1011;
+// 8진수 
+int iii= 013;
+//16 진수
+int iiii = 0xB3;
+
+
+
+
+public void type6(){
+
+
+    boolean res = true;
+
+if(true){
+    System.out.println("true");
+}
+
+if(false){
+    System.out.println("false");
+}
+
+
+}
+
+
+}
+
+
+
+
+
+
