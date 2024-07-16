befa70b1896572fd748e2ac90fb19f0c92fb2631
지후 <momo.class>
2024-07-16 10:48:42 +0900

    나의 첫 커밋

---
+++ src/App.java
@@ -0,0 +1,10 @@
+import com.human.ex01.Ex01;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        //System.out.println("Hello, World!");
+        Ex01 e = new Ex01();
+        e.print();
+    }
+
+}
